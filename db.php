4b49b1efb0ee97f760e9f43ee01216504ca12a9d
Bajojo01 <209912647+Bajojo01@users.noreply.github.com>
2026-05-17 17:37:29 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php 
-    $host = "localhost";
+    $host = "mysql.railway.internal";
     $username = "root";
-    $password = "";
-    $database = "jilz_perfume_bar";
+    $password = "HbeDhrdaesrLUVfjlagZkPGKQNtKHtGz";
+    $database = "railway";
 
     $conn = mysqli_connect($host, $username, $password, $database);
 
